1abcba88f0d8ce9dd21468a47bb2d814094b59d5
tenzin <ten13zin@gmail.com>
2016-09-07 23:51:34 +0530

    Fix typos and add eg code for array of object

--- contactInfo.js
+++ contactInfo.js
@@ -1,7 +1,7 @@
 var friends = new Object(); // constructor to create object friends
 
 // Objects into object
-friends.bill = {            //Lateral Notation to create object bill
+friends.bill = {            //Literal Notation to create object bill
      firstName : "Bill",
      lastName : "Gates",
      number : 5434-5454,
@@ -35,5 +35,62 @@ var search = function (name) {
         }
     }    
 };
+
+//Array of objects 
+
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+// create array of objects
+var contacts = [bob, mary]; 
+
+// log full name of a person to the console
+function printPerson(person) { 
+    console.log(person.firstName + " " + person.lastName); 
+}
+
+// log full of all the person in array of the object
+function list() {  
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+var search = function (lastName) {
+      var contactsLength = contacts.length;
+      for(var i = 0; i < contactsLength; i++ ) {
+            if (contacts[i].lastName === lastName) {
+                printPerson(contacts[i]);    
+            }    
+      }
+};
+
+//add new person to the array of object
+var add = function (firstName, lastName, email, phoneNumber) {
+    contacts[contacts.length] = { // main logic
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        email: email
+    }
+    list(contacts[contacts.length]);
+};
+
+// call add function
+add("Tenzin", "Maetok", "tentok@tenzin.mae", "9882204404");
 
 search("tenzin");
